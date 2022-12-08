2f200478757f80d79565511a3f8a106e87e135bc
Servule <dev.fosk@gmail.com>
2022-12-08 15:31:18 -0800

    Betty style delete whitespace

--- 0x00-hello_world/4-puts.c
+++ 0x00-hello_world/4-puts.c
@@ -1,6 +1,10 @@
 #include <stdio.h>
 
-/* Betty style: Print with puts */
+/**
+ * main - Entry point
+ *
+ * Return: Always 0 (Success)
+ */
 int main(void)
 {
 	puts("\"Programming is like building a multilingual puzzle");
