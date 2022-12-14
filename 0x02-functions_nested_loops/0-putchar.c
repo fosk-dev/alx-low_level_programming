d0e2ccd1a988d63ebcd7e7c312567176fedb28ba
Servule <dev.fosk@gmail.com>
2022-12-14 13:03:33 -0800

    fix: mandatory tasks

--- 0x02-functions_nested_loops/0-putchar.c
+++ 0x02-functions_nested_loops/0-putchar.c
@@ -2,11 +2,12 @@
 
 /**
  * main
+ *
  * Description: It prints the word _putchar, followed by a new line.
  * Return: 0.
  */
 int main(void)
 {
-	printf("_putchar\n");
+	printf("_putchar");
 	return (0);
 }
