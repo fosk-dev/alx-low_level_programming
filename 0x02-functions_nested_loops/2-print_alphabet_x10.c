dbde8b554ebe75615d2c8f091a82246d3c1316e1
servule <dev.fosk@gmail.com>
2022-12-14 21:43:44 +0000

    fix: mandatory tasks done

--- 0x02-functions_nested_loops/2-print_alphabet_x10.c
+++ 0x02-functions_nested_loops/2-print_alphabet_x10.c
@@ -6,7 +6,6 @@
  * alphabet in lowercase 10 times
  * Return: Nothing.
  */
-
 void print_alphabet_x10(void)
 {
 	char i;
--- 0x02-functions_nested_loops/3-islower.c
+++ 0x02-functions_nested_loops/3-islower.c
@@ -1,7 +1,7 @@
 #include "main.h"
 
 /**
- * _islower
+ * _islower in main file
  * @c: An input character
  * Description: function uses _putchar function to print
  * alphabet in lowercase 10 times
