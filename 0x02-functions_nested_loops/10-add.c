eb42e7e3425c5c4abf043b3532f34c8f067aac22
Servule <dev.fosk@gmail.com>
2022-12-14 13:18:17 -0800

    fix: mandatory tasks

--- 0x02-functions_nested_loops/10-add.c
+++ 0x02-functions_nested_loops/10-add.c
@@ -2,6 +2,8 @@
 
 /**
  * add - Check description
+ * @a: An integer a
+ * @b: An integer b
  * Description: Function that adds two integers and returns the result
  * Return: Result to add a and b
  */
